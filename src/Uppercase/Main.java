2b4025057bbefc5c0c29b7627e8f658fa9f29ab0
tjsk002 <tjsk002@naver.com>
2021-03-18 23:11:13 +0900

    대문자 소문자 뺼셈으로 추가

---
+++ src/Uppercase/Main.java
@@ -0,0 +1,31 @@
+package Uppercase;
+
+public class Main {
+	public static void main(String[] args) {
+		/*
+		 * 대문자는 소문자로, 소문자는 대문자로 변환하시오
+		 * helloWorLD
+		 * -> HELLOWORld
+		 * 
+		 * 배열, for, if
+		 * */
+		
+		String input = "helloWorLD";
+		char []arr;
+		//아스키 코드? 
+		arr = input.toCharArray();
+		
+		for(int i=0; i<arr.length; i++) {
+			//대문자냐 소문자냐 구분먼저?
+			//소문자 a부터 z까지 / A - Z
+			if(arr[i] >= 'a' && arr[i] <= 'z') {
+				arr[i] = (char) (arr[i] + 'A' - 'a');
+				
+			}else if(arr[i] >= 'A' && arr[i] <= 'Z'){
+				arr[i] = (char) (arr[i] - ('A' - 'a'));
+			}
+		}
+		System.out.println(arr);
+		
+	}
+}
