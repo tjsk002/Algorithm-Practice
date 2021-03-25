abb898d53899387233dac878d40c5462afa0da84
tjsk002 <tjsk002@naver.com>
2021-03-25 17:35:07 +0900

    factorial 계산법 연습

---
+++ src/Factorial/Main.java
@@ -0,0 +1,25 @@
+package Factorial;
+
+public class Main {
+	public static void main(String[] args) {
+		// 입력된 수의 팩토리얼을 구하시오
+		// 5 -> 120 (for, while)
+
+		// 5*4*3*2*1 = 120
+		// 재귀함수, 또는 반복문
+
+		int inputNum = 5;
+		// 5*4*3*2*1 = 120
+		// 7*6*5*4*3*2*1
+		int accNum = 1;
+		//초기화 0으로 보통하지만, 곱셈이다 보니 1이어야한다
+		
+		
+
+		// 마지막 i가 5가 되었을때 저장한 5와 5를 곱한 ..?
+		// 변수는 5 팩토리얼..?
+	
+	
+
+	}
+}
