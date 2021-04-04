68646350e2112ebfbcff0e97a398140401650feb
tjsk002 <tjsk002@naver.com>
2021-04-04 12:33:42 +0900

    arr

--- src/Array/Exam2.java
+++ src/Array/Exam2.java
@@ -2,22 +2,20 @@ package Array;
 
 class Parent {
 	int compute(int num) {
-		if (num <= 1) 
-				return num;
-		
+		if (num <= 1)
+			return num;
 		return compute(num - 1) + compute(num - 2);
-	
 	}
 }
 
 class Child extends Parent {
 	@Override
 	int compute(int num) {
-		if(num <= 1)return num;
-	return compute(num-1) + compute(num-3);	
+		if (num <= 1)
+			return num;
+		return compute(num - 1) + compute(num - 3);
 	}
 }
-
 
 public class Exam2 {
 	public static void main(String[] args) {
