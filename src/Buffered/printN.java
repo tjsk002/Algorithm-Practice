793278480860afb464a2e152a3ea537bb3113341
tjsk002 <tjsk002@naver.com>
2021-11-10 03:19:38 +0900

    beak joon 2742 Q : 기찍N

---
+++ src/Buffered/printN.java
@@ -0,0 +1,27 @@
+package Buffered;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class printN {
+	public static void main(String[] args) throws IOException{
+		/*
+		  input 3
+		  output
+		  3
+		  2
+		  1
+		*/
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 3
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		while(n>0) {
+			System.out.println(n);
+			n--;
+		}
+	}
+}
