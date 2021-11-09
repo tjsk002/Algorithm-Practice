86e40c493a4a3689a65b065fbf85ec42034a825d
tjsk002 <tjsk002@naver.com>
2021-11-10 01:58:18 +0900

    BufferedGugudan

--- src/Buffered/BufferedGugudan.java
+++ src/Buffered/BufferedGugudan.java
@@ -8,7 +8,7 @@ public class BufferedGugudan {
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(bf.readLine());
-		
+		//gugudan
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(n + " * " + i + " = " + (n*i));
 		}
