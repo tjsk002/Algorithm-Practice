d857a72302b11bfce2aeb55069dd90aaef39c2e3
tjsk002 <tjsk002@naver.com>
2021-11-10 03:41:57 +0900

    cat 출력하기

---
+++ src/CatDog/Cat.java
@@ -0,0 +1,17 @@
+package CatDog;
+
+public class Cat {
+	public static void main(String[] args) {
+		/*
+		\    /\
+		 )  ( ')
+		(  /  )
+		 \(__)|
+		Ãâ·Â
+		*/
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
