934834880091cf2ad1dbc339db3094776ccbf8bf
tjsk002 <tjsk002@naver.com>
2021-11-12 23:28:58 +0900

    최소값 최대값 구하기

---
+++ src/Scanner/highLowNum.java
@@ -0,0 +1,26 @@
+package Scanner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class highLowNum {
+	public static void main(String[] args) {
+		/*
+		 * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+		 * 5
+		 * 20 10 35 30 7
+		 * ----
+		 * 7 35
+		 */
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+//		sc.close();
+		Arrays.sort(arr);
+		System.out.println(arr[0] + " " + arr[N-1]);
+	}
+}
