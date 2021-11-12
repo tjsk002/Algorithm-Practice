684ad13baa30a9ec8adcfcc2f2f7670969ed1088
tjsk002 <tjsk002@naver.com>
2021-11-13 01:13:36 +0900

    temp

--- src/Scanner/HighLowNum.java
+++ src/Scanner/HighLowNum.java
@@ -3,7 +3,7 @@ package Scanner;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class highLowNum {
+public class HighLowNum {
 	public static void main(String[] args) {
 		/*
 		 * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
