eb7d69b6d3bed2fe782c7561268167f4bc105a22
tjsk002 <tjsk002@naver.com>
2021-11-16 02:59:37 +0900

    temp

---
+++ src/Scanner/SumCycle.java
@@ -0,0 +1,16 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class SumCycle {
+	public static void main(String[] args) {
+		// 더하기 사이클 beck joon Q
+		Scanner sc = new Scanner(System.in);
+		int originNum= sc.nextInt(); //주어진 수
+//		int NewNum = sc.nextInt(); //새로운 수
+		
+		int NewNum = ((originNum%10)*10)+((originNum/10)+(originNum%10)%10);
+		System.out.println(NewNum);
+		
+	}
+}
