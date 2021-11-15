1ab81acf034b3d387a8120a735652134c3640103
tjsk002 <tjsk002@naver.com>
2021-11-15 18:05:07 +0900

    beck joon Q : 정수 입력 9개 하고, 최대값과 몇번째인지 구하기

--- src/Scanner/MaxNumberth.java
+++ src/Scanner/MaxNumberth.java
@@ -1,8 +1,27 @@
 package Scanner;
 
+import java.util.Scanner;
+
 public class MaxNumberth {
 	public static void main(String[] args) {
 		// 최대값을 구하고 몇번째에 있는지 구하시오
-		int arr[] = new int[9];
+		
+		Scanner sc = new Scanner(System.in);
+		// 9개 정수 입력할 배열
+		int[] arr = {sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(),
+				sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()};
+		int count = 0; // 비교하기 위해
+		int max = 0; // 최대값
+		int index = 0; // 배열에 있는 index
+		
+		// 입력한 배열의 원소를 i에 저장하기
+		for (int i : arr) {
+			count++;
+			if(i > max) {
+				max = i;
+				index = count;
+			}
+		}
+		System.out.println(max + " " + index);
 	}
 }
