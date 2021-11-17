046cc8fcec201cee6364074349dcc6a4f15298e6
tjsk002 <tjsk002@naver.com>
2021-11-17 20:42:32 +0900

    remainder : HashSet 중복값 체크

--- src/Scanner/Remainder.java
+++ src/Scanner/Remainder.java
@@ -1,8 +1,15 @@
 package Scanner;
 
+import java.util.HashSet;
+
 public class Remainder {
 	public static void main(String[] args) {
 		// remainder : ³ª¸ÓÁö
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(1);
+		set.add(2);
+		set.add(1);
 		
+		System.out.println(set);
 	}
 }
