548239406fc6493e16a1aa60c7e8a2fbbfb4b4e9
tjsk002 <tjsk002@naver.com>
2021-11-17 20:51:26 +0900

    temp

--- src/Scanner/Remainder.java
+++ src/Scanner/Remainder.java
@@ -1,15 +1,26 @@
 package Scanner;
 
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Remainder {
 	public static void main(String[] args) {
 		// remainder : 나머지
-		HashSet<Integer> set = new HashSet<Integer>();
-		set.add(1);
-		set.add(2);
-		set.add(1);
+		/*
+		두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 
+		예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
+		수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 
+		그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 		
-		System.out.println(set);
+		첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+		*/
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			// 10자리 수
+			set.add(sc.nextInt() % 42);
+		}
+		System.out.println(set.size());
 	}
 }
