1dffd0a96eab93527a67ea791c22491624e225c7
tjsk002 <tjsk002@naver.com>
2021-12-19 23:37:22 +0900

    selfNumber

---
+++ Java-Algorithm-Practice/src/Scanner/SelfNumber.java
@@ -0,0 +1,17 @@
+package Scanner;
+public class SelfNumber {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			int n = d(i);
+			System.out.println(n);
+		}
+		System.out.println("");
+	}
+	
+	
+	public static int d (int number) {
+		number = 10;
+		int sum = number;
+		return sum;
+	}
+}
