9871d0487474f89ccbf8cf34c6f2a9b7ca23c887
tjsk002 <tjsk002@naver.com>
2021-12-20 16:47:02 +0900

    number sort

---
+++ Java-Algorithm-Practice/src/Array/NumberSort.java
@@ -0,0 +1,14 @@
+package Array;
+import java.util.Scanner;
+public class NumberSort {
+	public static void main(String[] args) {
+		// 수 정렬하기
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[sc.nextInt()];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println();
+	
+	}
+}
