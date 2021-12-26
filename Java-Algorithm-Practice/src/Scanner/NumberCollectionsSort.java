6b7295b7593e6ac2b8462e093cbe070e6330db8b
tjsk002 <tjsk002@naver.com>
2021-12-26 15:37:19 +0900

    Number Collections.sort() Scanner

--- Java-Algorithm-Practice/src/Scanner/NumberCollectionsSort.java
+++ Java-Algorithm-Practice/src/Scanner/NumberCollectionsSort.java
@@ -1,22 +1,23 @@
 package Scanner;
 
 import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class NumberCollectionsSort {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		List<Integer> num = new ArrayList<>();
+		int n = sc.nextInt(); // 처음 숫자 입력
+		StringBuilder sb = new StringBuilder();
+		
+		ArrayList<Integer> list = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
-			num.add(sc.nextInt());
+			list.add(sc.nextInt()); // 숫자 입력
 		}
-		Collections.sort(num);
-
-		for (int result : num) {
-			System.out.println(result);
+		Collections.sort(list);
+		for(int result : list) {
+			sb.append(result).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
