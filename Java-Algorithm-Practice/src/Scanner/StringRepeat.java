13a8c7d160847f657649184d0545d424c143e672
tjsk002 <tjsk002@naver.com>
2021-12-27 19:30:43 +0900

    String repeat

--- Java-Algorithm-Practice/src/Scanner/StringRepeat.java
+++ Java-Algorithm-Practice/src/Scanner/StringRepeat.java
@@ -7,11 +7,11 @@ public class StringRepeat {
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt(); // 맨처음 숫자
-		
+
 		for (int i = 0; i < T; i++) {
 			int R = sc.nextInt(); // 두번째 입력 숫자
 			String S = sc.next(); // String 입력
-			
+
 			for (int j = 0; j < S.length(); j++) {
 				for (int j2 = 0; j2 < R; j2++) {
 					System.out.print(S.charAt(j));
