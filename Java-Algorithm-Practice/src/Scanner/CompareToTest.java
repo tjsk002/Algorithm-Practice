27cb7ec48941783a4c7b0bec6de1ecec371d2c72
tjsk002 <tjsk002@naver.com>
2021-12-29 20:05:25 +0900

    compareTo

---
+++ Java-Algorithm-Practice/src/Scanner/CompareToTest.java
@@ -0,0 +1,10 @@
+package Scanner;
+
+public class CompareToTest {
+	public static void main(String[] args) {
+		String str = "abcd";
+		System.out.println(str.compareTo("ab"));
+		System.out.println(str.compareTo("b"));
+		System.out.println(str.compareTo("c"));
+	}
+}
