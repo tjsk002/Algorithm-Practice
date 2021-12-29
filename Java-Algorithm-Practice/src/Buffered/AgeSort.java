bb2e52ac2275c8e3277a522f9eb79736b6c20beb
tjsk002 <tjsk002@naver.com>
2021-12-29 20:04:55 +0900

    age sort 나이순서, 가입순서

---
+++ Java-Algorithm-Practice/src/Buffered/AgeSort.java
@@ -0,0 +1,21 @@
+package Buffered;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AgeSort {
+	public static void main(String[] args) throws IOException{
+		// 백준 나이순 정렬 문제
+		/**
+		 * 3
+		 * 21 Junkyu
+		 * 21 Dohyun
+         * 20 Sunyoung
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(" ");
+		int n = Integer.parseInt(br.readLine());
+	}
+}
