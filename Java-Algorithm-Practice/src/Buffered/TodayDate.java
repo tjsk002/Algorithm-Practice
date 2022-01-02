acf2b5329cc9c046c65ed571da9327c3fffda352
tjsk002 <tjsk002@naver.com>
2022-01-02 21:03:31 +0900

    today date

---
+++ Java-Algorithm-Practice/src/Buffered/TodayDate.java
@@ -0,0 +1,13 @@
+package Buffered;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TodayDate {
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		Date date = new Date();
+		System.out.println(sdf.format(date));
+	}
+}
