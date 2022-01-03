9e87afdf7f4604910ca1d6638b2772d2c55be3f5
tjsk002 <tjsk002@naver.com>
2022-01-03 20:46:23 +0900

    word count

---
+++ Java-Algorithm-Practice/src/Buffered/WordCount.java
@@ -0,0 +1,11 @@
+package Buffered;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class WordCount {
+	public static void main(String[] args) {
+		// 백준 단어의 개수
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
