9fdbce5676c6b2edef5a829569d44edc4a0f2db3
tjsk002 <tjsk002@naver.com>
2022-01-03 20:46:07 +0900

    String Number Sum

---
+++ Java-Algorithm-Practice/src/Scanner/StringNumberSum.java
@@ -0,0 +1,26 @@
+package Scanner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringNumberSum {
+	public static void main(String[] args) {
+		// 123 -> 6
+		// 987 -> 24
+//		String str = "안녕하세요";
+//		String[] arr = str.split("");
+//		System.out.println(Arrays.toString(arr));
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] arr = String.valueOf(n).split("");
+		// 숫자를 String으로 형변환 먼저해준다
+//		System.out.println(Arrays.toString(arr));
+		int sum=0;
+		for(String result : arr) {
+			sum += Integer.parseInt(result);
+		}
+		System.out.println(sum);
+		
+	}
+}
