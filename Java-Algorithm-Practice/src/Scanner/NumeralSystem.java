aadcc837a7b3f674ef87e58fe75985c9138973c3
tjsk002 <tjsk002@naver.com>
2022-01-03 22:34:35 +0900

    numeral system 진법 변환

--- Java-Algorithm-Practice/src/Scanner/NumeralSystem.java
+++ Java-Algorithm-Practice/src/Scanner/NumeralSystem.java
@@ -1,8 +1,17 @@
 package Scanner;
 
+import java.util.Scanner;
+
 public class NumeralSystem {
 	public static void main(String[] args) {
 		// numeral system -> Áø¹ı
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = "";
+		while(n > 0) {
+			str = n%2 + str;
+			n /= 2;
+		}
+		System.out.println(str);
 	}
 }
