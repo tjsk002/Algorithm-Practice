061cf26bae513e4e009932f855f17ff183cd680b
tjsk002 <tjsk002@naver.com>
2022-01-05 18:02:37 +0900

    핸드폰 맨뒤 숫자 4글자만 보이게끔 만들기 프로그래머스 문제

---
+++ Java-Algorithm-Practice/src/Programers/HandPhoneNumber.java
@@ -0,0 +1,25 @@
+package Programers;
+
+public class HandPhoneNumber {
+	public static void main(String[] args) {
+		// charAt 사용해서 풀어보기
+		String phone_number = "01033334444";
+		String answer = "";
+		// 다른 방법 정규식
+		String phoneNum = "0212341223";
+		String result = "";
+		
+		result = phoneNum.replaceAll(".(?=.{4})", "*");
+		
+		System.out.println("정규식으로 풀기 = " + result);
+		// return -> *******4444
+		for (int i = 0; i < phone_number.length(); i++) {
+			if(i<phone_number.length()-4) {
+				answer += "*";
+			}else {
+				answer += phone_number.charAt(i);
+			}
+		}
+		System.out.println("charAt()으로 풀기 = " + answer);
+	}
+}
