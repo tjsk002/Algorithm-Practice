b6f832fe57dad27b38a94140a142adc82ca835f0
tjsk002 <tjsk002@naver.com>
2022-01-06 15:14:38 +0900

    array sum

---
+++ Java-Algorithm-Practice/src/Programers/ArraysSum.java
@@ -0,0 +1,10 @@
+package Programers;
+
+public class ArraysSum {
+	public static void main(String[] args) {
+		// Çà·ÄÀÇ µ¡¼À
+		int[][] arr1 = new int[2][2];
+		int[][] arr2 = new int[2][2];
+		
+	}
+}
