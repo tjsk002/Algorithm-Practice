ddfd454374e3f6befd9204a29b5987baee3027b8
tjsk002 <tjsk002@naver.com>
2022-01-10 17:32:23 +0900

    자연수 뒤집어 배열로 만들기 long -> String = String.valueOf()

--- Java-Algorithm-Practice/src/Programers/ArrayReverse.java
+++ Java-Algorithm-Practice/src/Programers/ArrayReverse.java
@@ -4,20 +4,25 @@ import java.util.Arrays;
 
 public class ArrayReverse {
 	public static void main(String[] args) {
-		int n = 12345;
+		int intN = 12345;
+		long longN = 12345;
+		// long -> String = String.valueOf(); 사용
 		int[] answer = {};
 		
 		// [5,4,3,2,1] 출력
-		String str = Integer.toString(n);
+		String str = Integer.toString(intN);
 		StringBuilder sb = new StringBuilder(str);
 		
 		sb = sb.reverse();
 		System.out.println(sb);
 		
 		String[] strArr = sb.toString().split("");
-		System.out.println(Arrays.deepToString(strArr));
+		
+		System.out.println("strArr = " + Arrays.deepToString(strArr));
 		
 		answer = new int[sb.length()];
+//		System.out.println(answer.length);
+//		System.out.println(sb.length());
 		
 		for (int i = 0; i < sb.length(); i++) {
 			answer[i] = Integer.parseInt(strArr[i]);
