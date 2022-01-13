5176a81019946b676f22dfbeae1c704e3d474740
tjsk002 <tjsk002@naver.com>
2022-01-13 23:33:53 +0900

    happy number

---
+++ Java-Algorithm-Practice/src/Programers/HappyNum.java
@@ -0,0 +1,25 @@
+package Programers;
+
+import java.util.*;
+
+public class HappyNum {
+	public static boolean isHappy(int n) {
+		final Set<Integer> visit = new HashSet<>();
+
+		do {
+			visit.add(n);
+			int next = 0;
+			while (n > 0) {
+				int mod = n%10;
+				next += mod * mod;
+				n /= 10;
+			}
+			n = next;
+		} while(n != 1 && !visit.contains(n));
+		
+		return n == 1;
+	}
+	public static void main(String[] args) {
+		System.out.println(isHappy(19));
+	}
+}
