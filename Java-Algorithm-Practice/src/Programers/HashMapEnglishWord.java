24af655547a1d7a357fe70fd5c01685f0feb72f7
tjsk002 <tjsk002@naver.com>
2022-01-19 20:54:52 +0900

    hash map word test

---
+++ Java-Algorithm-Practice/src/Programers/HashMapEnglishWord.java
@@ -0,0 +1,20 @@
+package Programers;
+
+import java.util.HashMap;
+
+public class HashMapEnglishWord {
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("apple", "사과");
+		map.put("banana", "바나나");
+		map.put("orange", "오렌지");
+		map.put("melon", "멜론");
+		System.out.println(map);
+		for(String key : map.keySet()) {
+			System.out.printf("key : %s , value : %s \n", key, map.get(key));
+		}
+		map.remove("melon");
+		System.out.println(map); // {banana=바나나, orange=오렌지, apple=사과}
+		System.out.println(map.keySet()); // [banana, orange, apple]
+	}
+}
