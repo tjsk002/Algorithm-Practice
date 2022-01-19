7401470b3cdb83a8f90d109035bc82cd34041b33
tjsk002 <tjsk002@naver.com>
2022-01-19 22:40:22 +0900

    hash map

--- Java-Algorithm-Practice/src/Programers/HashMapEnglishWord.java
+++ Java-Algorithm-Practice/src/Programers/HashMapEnglishWord.java
@@ -1,6 +1,7 @@
 package Programers;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class HashMapEnglishWord {
 	public static void main(String[] args) {
@@ -16,5 +17,8 @@ public class HashMapEnglishWord {
 		map.remove("melon");
 		System.out.println(map); // {banana=바나나, orange=오렌지, apple=사과}
 		System.out.println(map.keySet()); // [banana, orange, apple]
+	
+		Iterator<String> key = map.keySet().iterator();
+		System.out.println("key = " + map.get(key));
 	}
 }
