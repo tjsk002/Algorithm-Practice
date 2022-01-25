2633bfe24d1242357d5a956e869e4a76a1d6e5ee
tjsk002 <tjsk002@naver.com>
2022-01-25 20:20:44 +0900

    print N

--- Java-Algorithm-Practice/src/Buffered/printN.java
+++ Java-Algorithm-Practice/src/Buffered/printN.java
@@ -1,10 +1,7 @@
 package Buffered;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class printN {
 	public static void main(String[] args) throws IOException{
