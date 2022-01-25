deabf955d4efea9b9ab2211157a2f47a1f5cd0fd
tjsk002 <tjsk002@naver.com>
2022-01-25 20:20:54 +0900

    사각 별 만들기

---
+++ Java-Algorithm-Practice/src/Programers/QuadrangleStar2.java
@@ -0,0 +1,17 @@
+package Programers;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class QuadrangleStar2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		IntStream.range(0, a).forEach(s -> sb.append("*"));
+		IntStream.range(0, b).forEach(s -> System.out.println(sb.toString()));
+	
+	}
+}
