c8c1aff7e4cc5aa083c815fc5d60d1bf2f1b5ea7
tjsk002 <tjsk002@naver.com>
2022-01-26 21:58:47 +0900

    two number sum

--- Java-Algorithm-Practice/src/Programers/TwoNumSum.java
+++ Java-Algorithm-Practice/src/Programers/TwoNumSum.java
@@ -1,15 +1,30 @@
 package Programers;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoNumSum {
 	public static void main(String[] args) {
 		// 두개 뽑아서 더하기
-		int[] numbers = {2,1,3,4,1};
+		int[] nums = { 2, 1, 3, 4, 1 };
 //		int[] result = {2,3,4,5,6,7};
-		int[] temp = {};
-		int a,b,c;
-		
-		for (int i = 0; i < numbers.length; i++) {
-//			temp[i] = 
+
+		int[] answer;
+
+		Set<Integer> set = new HashSet<Integer>();
+
+		for (int i = 0; i < nums.length; i++) {
+			// nums.length = 5
+//			System.out.println(i);
+			System.out.println();
+			for (int j = i + 1; j < nums.length; j++) {
+				set.add(nums[i] + nums[j]);
+//				System.out.println(set);
+			}
 		}
+		
+		answer = set.stream().sorted().mapToInt(Integer::intValue).toArray();
+		System.out.println(Arrays.toString(answer));
 	}
 }
